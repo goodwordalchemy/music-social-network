c9fff2f162e4a5d732a147483e2df55ee3a8c725
David Goldberg <goodwordachemy@gmail.com>
2017-11-21 17:35:02 -0800

    added cors package to app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, render_template
+from flask_cors import CORS
 
 from models import get_session, Track, User
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 def index():
