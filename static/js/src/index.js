a3dc405f6acccfc57cb67f32f586f625ce2c9a02
David Goldberg <goodwordachemy@gmail.com>
2017-11-21 17:36:36 -0800

    prove I can do an api request to my cors-enabled server with javascript's fetch api

--- static/js/src/index.js
+++ static/js/src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+APP_BASE_URL = 'http://localhost:5000'
+
+
 class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+
+        var res = fetch('http://localhost:5000/user/1', {method: 'get'})
+            .then((data) => {
+                return data.text()
+            })
+            .then((text) => {
+                alert(text);
+                debugger;
+            })
+            .catch(() => alert('error'));
+    }
+
     render() {
         return (<h1>Hello, Music App</h1>);
     }
